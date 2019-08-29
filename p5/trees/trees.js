db26ff797a0d5e279fe41294e89640f769bb53ff
Andy Bibler <theadog47@gmail.com>
2019-08-29 14:30:22 -0500

    asdf

---
+++ p5/trees/trees.js
@@ -0,0 +1,33 @@
+function setup() {
+  // Sets the screen to be 720 pixels wide and 400 pixels high
+  createCanvas(720, 400);
+  background(0, 180, 255);
+  noStroke();
+
+  fill(0, 150, 0);
+  rect(0, 300, 720, 100);
+
+  fill(139, 69, 19);
+  rect(120, 150, 30, 200);
+
+  fill(255);
+  ellipse(350, 160, 160, 55);
+
+  fill(255);
+  ellipse(220, 70, 160, 55);
+
+  fill(0, 80, 0);
+  ellipse(80, 135, 120, 120);
+
+  fill(0, 85, 0);
+  ellipse(180, 135, 120, 120);
+
+  fill(0, 90, 0);
+  ellipse(130, 150, 120, 120);
+
+  fill(0, 95, 0);
+  ellipse(130, 100, 120, 120);
+
+  fill(255, 255, 0);
+  ellipse(550, 80, 72, 72);
+}
