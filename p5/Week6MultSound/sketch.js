f33b65dbe09b2165c324a6a668de13f884a76afc
Andy Bibler <theadog47@gmail.com>
2019-10-01 13:46:53 -0500

    party

--- p5/Week6MultSound/sketch.js
+++ p5/Week6MultSound/sketch.js
@@ -3,9 +3,9 @@ var myState = 0;
 
 
 function preload() {
-  song1 = loadSound('assets/donkey.mp3');
-  song2 = loadSound('assets/skate.mp3');
-  song3 = loadSound('assets/thrill.mp3');
+  song1 = loadSound('assets/party.mp3');
+  song2 = loadSound('assets/party2.mp3');
+  song3 = loadSound('assets/party3.mp3');
 
   // set each song to loop and then immediately pause it so it doesn't play
 
